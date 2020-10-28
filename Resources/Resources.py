ac4dc707716704312ec4001541c7e9bc59ad5950
Minh Tue Cung <65919397+MinhTueCung@users.noreply.github.com>
2020-10-28 13:52:51 +0100

    All Files

---
+++ Resources/Resources.py
@@ -0,0 +1,41 @@
+supported_languages_dictionary = {
+    1: 'English',
+    2: 'Spanish',
+    3: 'French',
+    4: 'German',
+    5: 'Arabic',
+    6: 'Vietnamese',
+    7: 'Russian',
+    8: 'Portuguese',
+    9: 'Japanese',
+    10: 'Italian',
+    11: 'Thai'
+}
+
+supported_languages_their_encodings_dictionary = {
+    'English': 'en',
+    'Spanish': 'es',
+    'French': 'fr',
+    'German': 'de',
+    'Arabic': 'ar',
+    'Vietnamese': 'vi',
+    'Russian': 'ru',
+    'Portuguese': 'pt',
+    'Japanese': 'ja',
+    'Italian': 'it',
+    'Thai': 'th'
+}
+
+ask_for_the_input_language_message = 'Please say the language input (in English)\nFor example: "English", "German", etc.'
+incomprehensible_input_message = "Sorry, i didn´t get that. Please try again"
+system_defected_message = 'Sorry, my speech service is down'
+language_not_match_settings_message = 'Sorry, your desired language does not match the languages\nyou chose in settings! Please try again'
+
+size_of_text_in_buttons = 15
+standard_width_of_a_button = 120
+standard_height_of_a_button = 50
+
+canvas_background_on_gui_width = 667
+canvas_background_on_gui_height = 610
+
+choose_2_languages_to_translate_message = 'Choose 2 languages to translate back and forth\nChoose the first language then hit "Save"'
